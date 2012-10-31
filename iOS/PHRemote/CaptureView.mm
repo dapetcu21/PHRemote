fc9aae7d6c163b33975d263ffc35ba9259c6298e
Marius Petcu <dapetcu21@gmail.com>
2012-10-31 18:58:45 +0200

    Forgot to enable multitouch

--- iOS/PHRemote/CaptureView.mm
+++ iOS/PHRemote/CaptureView.mm
@@ -7,57 +7,28 @@
 @implementation CaptureView
 @synthesize delegate;
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
-- (void)dealloc
-{
-    [super dealloc];
-}
-
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     for (UITouch* touch in touches)
-    {
         [delegate sendTouchPack:0 withTouch:touch inView:self];
-    }
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     for (UITouch* touch in touches)
-    {
         [delegate sendTouchPack:1 withTouch:touch inView:self];
-    }
 }
+
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
     for (UITouch* touch in touches)
-    {
         [delegate sendTouchPack:2 withTouch:touch inView:self];
-    }
 }
+
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     for (UITouch* touch in touches)
-    {
         [delegate sendTouchPack:3 withTouch:touch inView:self];
-    }
 }
 
 @end
